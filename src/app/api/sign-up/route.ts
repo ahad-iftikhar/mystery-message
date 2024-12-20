f37c12ad5c9cb048ce6f19e0695dd1410cb2f766
Ahad Iftikhar <ahadiftikhar77@gmail.com>
2024-12-20 22:32:42 +0500

    Signup functionality

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -10,6 +10,68 @@ export async function POST(request: NextRequest) {
 
   try {
     const { username, email, password } = await request.json();
+    const existingUserVerifedByUsername = await User.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingUserVerifedByUsername) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Username is already taken",
+        },
+        { status: 400 }
+      );
+    }
+
+    const existingUserByEmail = await User.findOne({ email });
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    if (existingUserByEmail) {
+      true;
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setDate(expiryDate.getHours() + 1);
+
+      const newUser = new User({
+        username,
+        email,
+        password: hashedPassword,
+        verifyCode,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessage: true,
+        messages: [],
+      });
+
+      await newUser.save();
+    }
+
+    //send verfication email
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode
+    );
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        { status: 500 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: false,
+        message: "User registered successfully. Please verfiy your email",
+      },
+      { status: 201 }
+    );
   } catch (error) {
     console.error("Error registering user", error);
     return NextResponse.json(
