e7e63a0a1ccb93ce5c213c49f5b39264e092dbe7
Ahad Iftikhar <ahadiftikhar77@gmail.com>
2025-01-18 10:38:32 +0500

    Navbar and MessageCard frontend and other bug fixes

---
+++ src/app/(app)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const page = () => {
+  return <div>Dashboard page</div>;
+};
+
+export default page;
---
+++ src/app/u/[username]/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const page = () => {
+  return <div>You page</div>;
+};
+
+export default page;
